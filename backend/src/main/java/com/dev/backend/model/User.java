668d90566cbef5182ec5450918dbcf25990eb41e
hkhlifi <khllifihamza66@gmail.com>
2025-09-03 10:02:57 +0100

    User entity & repository

---
+++ backend/src/main/java/com/dev/backend/model/User.java
@@ -0,0 +1,55 @@
+package com.dev.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity{
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
---
+++ backend/src/main/java/com/dev/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.dev.backend.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dev.backend.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID>{
+}
